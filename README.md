a560e557796dfbc0c6db0b89acdbc82857709906
Shawn <13626862366@163.com>
2021-07-05 14:19:40 +0800

    first init

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
-# access_code
+### ZSTU通行码
+进入src/views/Home.vue修改自己的姓名和学号，二维码图片在src/assets下,换图片后记得把命名修改为qr.jpg,因为是静态，该码不可刷
 
-## Project setup
-```
-yarn install
-```
+### 下载步骤
+1、安装依赖
+npm install
+2、运行
+npm run serve
+3、打包
+npm run build
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+最后将打包的东西放在服务器即可访问，最后的图片样式
 
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+![image-20210705141424105](pic.png)
--- src/router/index.js
+++ src/router/index.js
@@ -4,24 +4,32 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home,
+        meta: {
+            title: '学生通行码'
+        },
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
-
+router.beforeEach((to, from, next) => {
+    if (to.meta.title) {
+        document.title = to.meta.title;
+        next();
+    }
+})
 export default router
